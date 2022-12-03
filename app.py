ee6c891dcc6635f1480471d5d458fdf997510302
JC <juan.medina@csedge.org>
2022-12-03 16:23:00 -0500

    flask finish

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')       
 
-@app.route('/capture', methods=["POST"])
+@app.route('/capture', methods=["GET","POST"])
 def capture():
     camera = picamera.PiCamera()
     #use a timestamp to save pics with unique names based on seconds since 1970.
